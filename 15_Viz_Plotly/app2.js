6742942106e724f037f3381fb1e019ddc197e4d7
April Dominguez <aprildominguez@Aprils-MBP.attlocal.net>
2021-01-06 22:04:50 -0500

    update_html2_removal

--- 15_Viz_Plotly/app2.js
+++ 15_Viz_Plotly/app2.js
@@ -86,93 +86,51 @@ function buildCharts(samplechoice) {
 
         Plotly.newPlot('pie', pieData, pie_layout)
   
-        // Define gauge chart data - Bonus   
+        //    Define gauge chart data - Bonus   
         
-        // Get the data; define the y ticks; put the otu_ids in descending order via reverse function
+        // Got the data; define the y ticks; put the otu_ids in descending order via reverse function
       
             // d3.json(file).then((data) => {
   //       var samplevalues = sample[0].sample_values;
   //       var washFreq = metadata.slice(0,10).reverse().map(item => `OTU ${item}`);
         
-        var data = [
-    {
-      type: "scatter",
-      x: [0],
-      y: [0],
-      marker: { size: 12, color: "850000" },
-      showlegend: false,
-      name: "Freq",
-      text: level,
-      hoverinfo: "text+name"
-    },
-    {
-      values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
-      rotation: 90,
-      text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
-      textinfo: "text",
-      textposition: "inside",
-      marker: {
-        colors: [
-          "rgba(0, 105, 11, .5)",
-          "rgba(10, 120, 22, .5)",
-          "rgba(14, 127, 0, .5)",
-          "rgba(110, 154, 22, .5)",
-          "rgba(170, 202, 42, .5)",
-          "rgba(202, 209, 95, .5)",
-          "rgba(210, 206, 145, .5)",
-          "rgba(232, 226, 202, .5)",
-          "rgba(240, 230, 215, .5)",
-          "rgba(255, 255, 255, 0)"
-        ]
-      },
-      labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
-      hoverinfo: "label",
-      hole: 0.5,
-      type: "pie",
-      showlegend: false
-    }
-  ];
- white_check_mark
- eyes
- raised_hands
- 
- 
- 
- 
- 
- 
- var layout = {
-    shapes: [
-      {
-        type: "path",
-        path: path,
-        fillcolor: "850000",
-        line: {
-          color: "850000"
-        }
+  //       //console.log(sampleotuids);
+
+  //       var trace_g = [{
+  //         domain: {x: [0,1], y: [0, 1]},
+  //         value: parseInt(samplewfreq),
+  //         title: {text: "<b>Belly Button Washing Frequency </b><br> (Scrubs per Week)"},
+  //         type: "indicator",
+  //         mode: "gauge+number+delta",
+  //         gauge: {
+  //           axis: {range: [null, 9]},
+  //           steps: [
+  //             {range: [0,1], color: "#e5d5d0"},
+  //             {range: [1,2], color: "#dbc7c2"},
+  //             {range: [2,3], color: "teal"},
+  //             { range: [3, 4], color: "#c9ada7" },
+  //             { range: [4, 5], color: "#ac9899" },
+  //             { range: [5, 6], color: "#8a7e88" },
+  //             { range: [6, 7], color: "#7d7482" },
+  //             { range: [7, 8], color: "#706a7b" },
+  //             { range: [8, 9], color: "#4a4e69" }
+  //           ],
+  //         threshold: {
+  //           value: samplewfreq
+  //         }
+  //       }
+  //     }
+  // ];
+
+  //       var layout_g = {
+  //         width: 600,
+  //         height: 450,
+  //         margin: { t: 0, b: 0},
+  //         };
+  //         Plotly.newPlot("gauge", trace-g, layout_g);
+        });
       }
-    ],
-    title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
-    height: 500,
-    width: 500,
-    xaxis: {
-      zeroline: false,
-      showticklabels: false,
-      showgrid: false,
-      range: [-1, 1]
-    },
-    yaxis: {
-      zeroline: false,
-      showticklabels: false,
-      showgrid: false,
-      range: [-1, 1]
-    }
-  };
-  var GAUGE = document.getElementById("gauge");
-  Plotly.newPlot(GAUGE, data, layout);
- })
- 
- 
+
 
 // Define function for collecting metadata information
 function meta(samplechoice) {
