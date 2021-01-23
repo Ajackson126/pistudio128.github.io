5e04439db760c56d5ad37380edb98d96e9ac914e
April Dominguez <aprildominguez@Aprils-MBP.attlocal.net>
2021-01-22 20:06:35 -0500

    Update_Map_Circles

--- 17_Leaflet-Challenge/Leaflet-Step-1/static/js/logic.js
+++ 17_Leaflet-Challenge/Leaflet-Step-1/static/js/logic.js
@@ -4,7 +4,7 @@ var baseMap = L.map("map", {
     zoom: 5
 });
 
-// Create the tile layer that will be the background of our map
+// Add tile layer for the background of map
 var lightmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
@@ -12,89 +12,168 @@ var lightmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/
   accessToken: API_KEY
 }).addTo(baseMap);
 
-// Add earthquake data from url
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Store API query variables
+var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// var mag = "$where=created_date between'2018-01-01T00:00:00' and '2019-01-01T00:00:00'";
+// var depth = "&complaint_type=Rodent";
+// var loc = "&$limit=1000";
 
-// // Create a legend to display information about our map
-var info = L.control({
-   position: "bottomright" 
-});
+// Assemble API query URL
+// var url = baseURL + mag + depth + loc;
 
-// Create a function to get colors for circle markers
-function getColors(d) {
-    if (d < 1){
-        return "#B7DF5F"
-    }
-    else if (d < 2) {
-        return "#DCED11"
-    }
-    else if (d < 3) {
-        return "#EDD911"
-    }
-    else if (d <4){
-        return "#EDB411"
-    }
-    else if (d<5){
-        return "ED7211"
-    }
-    else {
-        return "ED4311"
-    }
-};
 
-// Create a function that creates markers
-function createCircleMarker(feature, latlng){
-
-// Change the symbol criteria
-    var markerOptions = {
-        radius: markerSize(feature.properties.mag),
-        fillColor: getColors(feature.properties.mag),
-        color: "black",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-    }
-
-    return L.circleMarker (latlng, markerOptions);
-};
-
-// Use d3 to get data from URL
-d3.json(queryUrl, function(data) {
-    console.log(data)
-
-    var earthquakes = data.features
-  
-    console.log(earthquakes)
+legend.addTo(map);
+// Grab data with d3
+d3.json(baseURL, function(response) {
+    // Create a new marker cluster group
+    console.log(response);
     
-    // loop through the data to create markers and popup
-    earthquakes.forEach(function(result){
 
-      //console.log(result.properties)
-      L.geoJSON(result,{
-        pointToLayer: createCircleMarker
+    // Loop through data
+    function getColors(d) {
+        if (d < 1){
+            return "#800026"
+        }
+        else if (d < 2) {
+            return "#BD0026"
+        }
+        else if (d < 3) {
+            return "#E31A1C"
+        }
+        else if (d <4){
+            return "#FC4E2A"
+        }
+        else if (d<5){
+            return "#FD8D3C"
+        }
+        else {
+            return "#FEB24C"
+        }
+    }
 
-        // add popups to the circle markers to display data
-      }).bindPopup("Date: " + new Date(result.properties.time) + "<br>Place: " + result.properties.place + "<br>Magnitude: " + result.properties.mag).addTo(myMap)
-    });
+        // // Loop through data
+        // function getRadius(e) {
+        //     if (e < 1){
+        //         return "#B7DF5F"
+        //     }
+        //     else if (e < 2) {
+        //         return "#DCED11"
+        //     }
+        //     else if (e < 3) {
+        //         return "#EDD911"
+        //     }
+        //     else if (e <4){
+        //         return "#EDB411"
+        //     }
+        //     else if (e<5){
+        //         return "ED7211"
+        //     }
+        //     else {
+        //         return "ED4311"
+        //     }
+        // }
+
+    L.geoJSON(response, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(
+                "Magnitude : " 
+                + feature.properties.mag
+                +"<br>"
+                + feature.properties.place
+                +"<br>"
+                + feature.geometry.coordinates[2]
+            )
+        
+        },
+        style: function(feature){
+            return{
+                "color":  "#white",
+                "weight" : 2,
+                "opacity":  0.65,
+                "dashArray": 0.7,
+                "fillColor" : getColors(feature.geometry.coordinates[2]),
+                //"radius": getRadius(feature.properties.mag[0])
+        }
+        }
+
+    }).addTo(baseMap);
+
+})
+
+
+    // L.geoJSON(response, {
+    //     pointToLayer: function (feature, latlng) {
+    //         return L.circleMarker(latlng);
+    //     }
+    // }).addTo(baseMap);
+// want size, depth and location
+// mag, 
+
+
+// // // Create a legend to display information about our map
+// var info = L.control({
+//    position: "bottomright" 
+// });
+
+
+
+// // Create a function that creates markers
+// function createCircleMarker(feature, latlng){
+
+// // Change the symbol criteria
+//     var markerOptions = {
+//         radius: markerSize(feature.properties.mag),
+//         fillColor: getColors(feature.properties.mag),
+//         color: "black",
+//         weight: 1,
+//         opacity: 1,
+//         fillOpacity: 0.8
+//     }
+
+//     return L.circleMarker (latlng, markerOptions);
+// };
+
+// // Use d3 to get data from URL
+// d3.json(queryUrl, function(data) {
+//     console.log(data)
+
+//     var earthquakes = data.features
   
-    //create legennds and add to the map
-    var legend = L.control({position: "bottomright" });
-    legend.onAdd = function(){
-      // create div for the legend
-      var div = L.DomUtil.create('div', 'info legend'),
-          grades = [0, 1, 2, 3, 4, 5]
-          labels = [];
+//     console.log(earthquakes)
+    
+//     // loop through the data to create markers and popup
+//     earthquakes.forEach(function(result){
+
+//       //console.log(result.properties)
+//       L.geoJSON(result,{
+//         pointToLayer: createCircleMarker
+
+//         // add popups to the circle markers to display data
+//       }).bindPopup("Date: " + new Date(result.properties.time) + "<br>Place: " + result.properties.place + "<br>Magnitude: " + result.properties.mag).addTo(myMap)
+//     });
   
-      // loop through our density intervals and generate a label with a colored square for each interval
-      for (var i = 0; i < grades.length; i++) {
-          div.innerHTML +=
-              '<i style="background:' + getColors(grades[i]) + '"></i> ' +
-              grades[i] + (grades[i +1 ] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-      }
-      return div;
-    };
-    legend.addTo(baseMap);
-  });
+//     //create legennds and add to the map
+//     var legend = L.control({position: "bottomright" });
+//     legend.onAdd = function(){
+//       // create div for the legend
+//       var div = L.DomUtil.create('div', 'info legend'),
+//           grades = [0, 1, 2, 3, 4, 5]
+//           labels = [];
+  
+//       // loop through our density intervals and generate a label with a colored square for each interval
+//       for (var i = 0; i < grades.length; i++) {
+//           div.innerHTML +=
+//               '<i style="background:' + getColors(grades[i]) + '"></i> ' +
+//               grades[i] + (grades[i +1 ] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+//       }
+//       return div;
+//     };
+//     legend.addTo(baseMap);
+//   });
 // })
 
 // // Initialize all of the LayerGroups we'll be using
@@ -180,81 +259,3 @@ d3.json(queryUrl, function(data) {
 //     shape: "circle"
 //   })
 // };
-
-// // Perform an API call to the Citi Bike Station Information endpoint
-// d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json", function(infoRes) {
-
-//   // When the first API call is complete, perform another call to the Citi Bike Station Status endpoint
-//   d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_status.json", function(statusRes) {
-//     var updatedAt = infoRes.last_updated;
-//     var stationStatus = statusRes.data.stations;
-//     var stationInfo = infoRes.data.stations;
-
-//     // Create an object to keep of the number of markers in each layer
-//     var stationCount = {
-//       COMING_SOON: 0,
-//       EMPTY: 0,
-//       LOW: 0,
-//       NORMAL: 0,
-//       OUT_OF_ORDER: 0
-//     };
-
-//     // Initialize a stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station count for layer group
-//     var stationStatusCode;
-
-//     // Loop through the stations (they're the same size and have partially matching data)
-//     for (var i = 0; i < stationInfo.length; i++) {
-
-//       // Create a new station object with properties of both station objects
-//       var station = Object.assign({}, stationInfo[i], stationStatus[i]);
-//       // If a station is listed but not installed, it's coming soon
-//       if (!station.is_installed) {
-//         stationStatusCode = "COMING_SOON";
-//       }
-//       // If a station has no bikes available, it's empty
-//       else if (!station.num_bikes_available) {
-//         stationStatusCode = "EMPTY";
-//       }
-//       // If a station is installed but isn't renting, it's out of order
-//       else if (station.is_installed && !station.is_renting) {
-//         stationStatusCode = "OUT_OF_ORDER";
-//       }
-//       // If a station has less than 5 bikes, it's status is low
-//       else if (station.num_bikes_available < 5) {
-//         stationStatusCode = "LOW";
-//       }
-//       // Otherwise the station is normal
-//       else {
-//         stationStatusCode = "NORMAL";
-//       }
-
-//       // Update the station count
-//       stationCount[stationStatusCode]++;
-//       // Create a new marker with the appropriate icon and coordinates
-//       var newMarker = L.marker([station.lat, station.lon], {
-//         icon: icons[stationStatusCode]
-//       });
-
-//       // Add the new marker to the appropriate layer
-//       newMarker.addTo(layers[stationStatusCode]);
-
-//       // Bind a popup to the marker that will  display on click. This will be rendered as HTML
-//       newMarker.bindPopup(station.name + "<br> Capacity: " + station.capacity + "<br>" + station.num_bikes_available + " Bikes Available");
-//     }
-
-//     // Call the updateLegend function, which will... update the legend!
-//     updateLegend(updatedAt, stationCount);
-//   });
-// });
-
-// // Update the legend's innerHTML with the last updated time and station count
-// function updateLegend(time, stationCount) {
-//   document.querySelector(".legend").innerHTML = [
-//     "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
-//     "<p class='out-of-order'>Out of Order Stations: " + stationCount.OUT_OF_ORDER + "</p>",
-//     "<p class='coming-soon'>Stations Coming Soon: " + stationCount.COMING_SOON + "</p>",
-//     "<p class='empty'>Empty Stations: " + stationCount.EMPTY + "</p>",
-//     "<p class='low'>Low Stations: " + stationCount.LOW + "</p>",
-//     "<p class='healthy'>Healthy Stations: " + stationCount.NORMAL + "</p>"
-//   ].join("");
-// }
